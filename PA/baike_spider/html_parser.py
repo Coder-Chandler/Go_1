4b30db15eb38314a870ce70adf7bff4dc48fd656
Chandler <ysiwgtus@gmail.com>
2017-06-08 17:20:48 +0800

    Html Parser

--- PA/baike_spider/html_parser.py
+++ PA/baike_spider/html_parser.py
@@ -1,3 +1,5 @@
+# !/usr/bin/python
+# -*- coding:utf-8 -*-
 # Html Parser
 from bs4 import BeautifulSoup
 import re
@@ -7,13 +9,13 @@ import urlparse
 class HtmlParser(object):
 
     # Sub-link style, we need to follow page_url to complete sub-link (Baidu entry sub-link style -->/item/.)
-    # Sub-link style, we need to follow page_url to complete sub-link (pinterest sub-link style -->href="/pin/249668373071793903/")
+    # Sub-link style, we need to follow page_url to complete sub-link (Wikipedia entry sub-link style -->/wiki/.)
     def _get_new_urls(self, page_url, soup):
         # We need to use set to filter the same data !
         new_urls = set()
         # find all the sub-links
-        links = soup.find_all('a', href=re.compile(r"^/pin/.+"))
-        print 'links of sub-link is OK !'
+        links = soup.find_all('a', href=re.compile(r"/wiki/.+"))
+        '''print "links of sub-link is OK !"'''
         for link in links:
             new_url = link['href']  # get the link
             new_full_url = urlparse.urljoin(page_url, new_url) # make sub-link complete
@@ -25,23 +27,17 @@ class HtmlParser(object):
 
         # Url
         res_data['url'] = page_url
-        print 'page_url has already add in res_data !'
+        '''print "page_url has already add in res_data !"'''
 
         # Title tag -> <dd class="lemmaWgt-lemmaTitle-title"><h1>Python</h1>  (Baidu entry)
-        # <div class="antialiased sans-serif text-lg _2c text-center bold truncate">All Pins</div> (Pinterest)
-
-        # <img id="i-f88v" src="https://s-media-cache-ak0.pinimg.com/236x/0a/ad/cc/0aadcce6c2daba0e0869e6fc6ee9649d.jpg"
-        # class="pinImg fullBleed loaded" alt="short curly bob hairstyle">  (Pinterest)
-        #img_node = soup.find('img', id=re.compile(r"^i-.+")).find(" ", src=re.compile(r"https://s-media-cache-ak0.pinimg.com/\d.+"))
-        img_node = soup.find(re.compile(r"https://s-media-cache-ak0.pinimg.com/\d.+?"))
-        # Add the img in res_data
-        res_data["img"] = img_node
-        print 'img_node has already add in res_data !'
+        # <h1 id="firstHeading" class="firstHeading" lang="zh-CN">人工智能
+        title_node = soup.find('h1')
+        # Add the XX_node in res_data
+        res_data["title"] = title_node.get_text()
+        '''print "title_node has already add in res_data !"'''
 
         '''
         # Summary tag -> <div class="lemma-summary" label-module="lemmaSummary"> (Baidu entry)
-        # <p class="pinDescription">great piece for the living room or bedroom. I love the...</p>
-        # <div class="pinMetaWrapper">
         summary_node = soup.find('div', class_="pinMetaWrapper")
         # Add the summary in res_data
         res_data["summary"] = summary_node.get_text()
