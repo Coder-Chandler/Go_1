a692eb58716e03697015a31d32f67fc9f4bb4c23
Coder-Chandler <ysiwgtus@gmail.com>
2017-06-12 21:31:05 +0800

    python re test

---
+++ retest/test/test.py
@@ -0,0 +1,14 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'chandler'
+
+import re
+
+#line = "xxx出生于2001年6月1日"
+#line = "xxx出生于2001/6/1"
+#line = "xxx出生于2001-6-1"
+#line = "xxx出生于2001-06-01"
+line = "xxx出生于2001-06月-01日"
+regex_str = ".*出生于(\d{4}[年/-]\d{1,2}(([月/-]|[月/-][月/-])\d{1,2}[日/-]|[月/-]\d{1,2}|[月/-]$|$))"
+match_obj = re.match(regex_str, line)
+if match_obj:
+    print (match_obj.group(1))
